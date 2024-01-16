216d36d67147e099a32a49251714730d75487554
CKS1210 <119333712+CKS1210@users.noreply.github.com>
2024-01-16 16:56:36 +0800

    Evaluation File added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.ChickenDiseaseClassification import logger
 from src.ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ChickenDiseaseClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.ChickenDiseaseClassification.pipeline.stage_03_training import ModelTrainingPipeline
+from src.ChickenDiseaseClassification.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -38,6 +39,14 @@ except Exception as e:
     raise e
 
 
-
+STAGE_NAME = 'Evaluation Stage'
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
